c5579515db46e1b9ad98ce8317d7415b85277113
Marcelo-Med <marcelo.medina.canto018@gmail.com>
2026-07-20 10:36:51 -0600

    First Commit

---
+++ src/test/java/com/footballtournament/management/FootballTournamentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballtournament.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTournamentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
