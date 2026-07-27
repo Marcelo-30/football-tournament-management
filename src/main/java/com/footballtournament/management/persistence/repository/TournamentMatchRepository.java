6f0006e650b1b91eda73ac1fa73ad049af3cdaf4
Marcelo-Med <marcelo.medina.canto018@gmail.com>
2026-07-27 10:55:20 -0600

    feat: add web dependency and custom repository queries

--- src/main/java/com/footballtournament/management/persistence/repository/TournamentMatchRepository.java
+++ src/main/java/com/footballtournament/management/persistence/repository/TournamentMatchRepository.java
@@ -4,8 +4,18 @@ import com.footballtournament.management.persistence.entity.TournamentMatchEntit
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TournamentMatchRepository
         extends CrudRepository<TournamentMatchEntity, Integer> {
+
+    List<TournamentMatchEntity> findByTournament_Id(Integer tournamentId);
+
+    List<TournamentMatchEntity>
+    findByHomeTeamIgnoreCaseOrAwayTeamIgnoreCase(
+            String homeTeam,
+            String awayTeam
+    );
 }
 
--- src/main/java/com/footballtournament/management/persistence/repository/TournamentRepository.java
+++ src/main/java/com/footballtournament/management/persistence/repository/TournamentRepository.java
@@ -4,7 +4,13 @@ import com.footballtournament.management.persistence.entity.TournamentEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TournamentRepository
         extends CrudRepository<TournamentEntity, Integer> {
+
+    List<TournamentEntity> findBySeasonIgnoreCase(String season);
+
+    List<TournamentEntity> findByNameContainingIgnoreCase(String name);
 }
