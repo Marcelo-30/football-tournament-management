67018f1aa8525838d61c46d43c2a4ba079ed3a76
Marcelo-Med <marcelo.medina.canto018@gmail.com>
2026-08-03 01:10:20 -0600

    refactor: relate tournament matches to registered teams

--- src/main/java/com/footballtournament/management/persistence/mapper/TournamentMatchMapper.java
+++ src/main/java/com/footballtournament/management/persistence/mapper/TournamentMatchMapper.java
@@ -2,7 +2,6 @@ package com.footballtournament.management.persistence.mapper;
 
 import com.footballtournament.management.domain.model.TournamentMatch;
 import com.footballtournament.management.persistence.entity.TournamentMatchEntity;
-import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
@@ -14,9 +13,15 @@ import org.mapstruct.ReportingPolicy;
 public interface TournamentMatchMapper {
 
     @Mapping(source = "id", target = "matchId")
+    @Mapping(source = "homeTeam.teamId", target = "homeTeamId")
+    @Mapping(source = "homeTeam.name", target = "homeTeamName")
+    @Mapping(source = "awayTeam.teamId", target = "awayTeamId")
+    @Mapping(source = "awayTeam.name", target = "awayTeamName")
     TournamentMatch toDomain(TournamentMatchEntity entity);
 
-    @InheritInverseConfiguration
+    @Mapping(source = "matchId", target = "id")
+    @Mapping(target = "homeTeam", ignore = true)
+    @Mapping(target = "awayTeam", ignore = true)
     @Mapping(target = "tournament", ignore = true)
     TournamentMatchEntity toEntity(TournamentMatch domain);
 }
